2687d68aa11f0861d45c90a5de21e7fdc5f0f924
Luiz Fernandes Trabalho <luiz.fernandes31matos@gmail.com>
2025-02-21 11:48:00 -0300

    Criação da página de projetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Pages
 import Home from './pages/Home'
 import About from './pages/About';
+import Projects from './pages/Projects';
+
+//Components
 import Navbar from './components/navbar';
 import Footer from './components/Footer';
 
@@ -13,6 +18,7 @@ function App() {
     <Routes> 
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
     </Routes>
     <Footer/>
   </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {  NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
 <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+  <div className="container-fluid mx-3">
     <NavLink className="navbar-brand fw-bold" to="/">Home</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -14,7 +14,7 @@ const Navbar = () => {
           <NavLink className="nav-link active" aria-current="page" to="/About">Sobre</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Projetos</a>
+        <NavLink className="nav-link active" aria-current="page" to="/Projects">Projetos</NavLink>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Ensino</a>
